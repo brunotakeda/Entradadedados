b7d8a686408626b315afcae709d034b2c64865db
brunotakeda <takedabruno@outlook.com>
2022-03-07 19:52:15 +0000

    finalizando por hoje

---
+++ ControleFor.java
@@ -0,0 +1,11 @@
+public class ControleFor {
+
+    public static void main(String[] args) {
+
+        for(int x = 0; x <1000; x++) {
+            System.out.print("valor: " + x);
+        }
+    }
+}
+
+  
---
+++ ControleFor1.java
@@ -0,0 +1,12 @@
+public class ControleFor1 {
+
+    public static void main(String[] args) {
+
+
+        int[] array = {1,2,3,4,5};
+        for (int i : array) {
+            
+            System.out.println(i);
+        }
+    }
+}
